f9cd44f21353cf8aaf5f152e6fd7ae8c73012fee
Dima Babai <dimababai2004@gmail.com>
2023-04-25 11:01:30 +0300

    final disign

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-60-1682407056.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-de49711d', {"layout":"number","heading":"\u0411\u0430\u043a\u0430\u043b\u0430\u0432\u0440","numberPrefix":"","numberSuffix":"","startNumber":0,"endNumber":75,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":"","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-6a028957', {"layout":"number","heading":"\u041c\u0430\u0433\u0456\u0441\u0442\u0440","numberPrefix":"","numberSuffix":"","startNumber":0,"endNumber":50,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBTabs.init( '.uagb-block-6d85f4e6' );
+	UAGBTabs.anchorTabId( '.uagb-block-6d85f4e6' );
+});
+window.addEventListener( 'hashchange', function() {
+	UAGBTabs.anchorTabId( '.uagb-block-6d85f4e6' );
+}, false );
+ });
---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-62-1682405490.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	var swiper = new Swiper( ".uagb-block-5add9466 .uagb-swiper",
+		{"autoplay":{"delay":3000,"disableOnInteraction":false,"pauseOnMouseEnter":true,"stopOnLastSlide":false},"loop":true,"speed":800,"effect":"slide","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":{"el":".uagb-block-5add9466 .swiper-pagination","clickable":true,"hideOnClick":false},"navigation":{"nextEl":".uagb-block-5add9466 .swiper-button-next","prevEl":".uagb-block-5add9466 .swiper-button-prev"}}	);
+});
+
+ });
